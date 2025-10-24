db00cdba79bcf5c447f238a964ddcb4d47f433a5
avishkar09-dev <avishkarbhosale519@gmail.com>
2025-10-24 20:54:57 +0530

    Add files via upload

---
+++ Basics 1.py
@@ -0,0 +1,22 @@
+
+a="pankaj"
+print(a[0])
+print(a[-6])
+print(a[1:5]) # start from index 1 till go to index 5-1 (1,2,3,4)
+print(a[1:])  # (1,2,3,4,5)
+print(a[:5])  # (0,1,2,3,4)
+print(a*2)    # multiply
+print(len(a)) # It will give length of the string
+print(a*(-2)) # It will print empty string
+print(int(12.45))
+print(float(12))
+print(int(True))
+print(float(True)) #It will give error
+# We can convert any type to float except imaginary
+print(complex(2)) #It will give complex number
+print(complex(12.5))
+print(complex(True))
+# print(complex("one")) It will give error
+print(bool(" "))
+#print(float(2+3j))
+print(a[-2:])
---
+++ Basics 2.py
@@ -0,0 +1,28 @@
+'''l=['A','B','C','D','E','F','G'] #List
+print(l[0])
+print(l[-1])
+print(l[1:])
+print(l[1:3])
+t=('A','B','C','D')
+print(type(t))
+for element in range(2,20,3):print(element)
+r=range(5,10)
+print(type(r))
+print(r[0])
+l=list(range(5,10))
+print(l)
+t=tuple(range(5,10))
+print(t)
+s=list("pankaj")
+print(s)
+s="Pankaj sharma is good faculty"
+l=list(s)
+print(l)
+d={1:'Avi',2:'Rushi',3:'Prince'}
+print(type(d))
+print(d)
+d[4]='Sham'
+print(d)'''
+d={}
+d[5]='sham'
+print(d)
