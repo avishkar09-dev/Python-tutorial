98e2002e0a47be16cc9a9a6550644dd758eb40d4
avishkar09-dev <avishkarbhosale519@gmail.com>
2025-10-28 20:43:52 +0530

    Add files via upload

---
+++ Operators1.py
@@ -0,0 +1,27 @@
+'''a=10
+b=20
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a**b)
+print(a%b)
+print('Avi'*2)
+print(a<b)
+a=100
+print(id(a))
+a=a+1
+print(id(a))
+a=int(input())
+b=int(input())
+print(a+b)
+print(10 and 20)
+print(0 and 20)
+print(20 and 0)
+print(10 or 20)
+print(0 or 20)
+print(20 or 0)'''
+print((5&6))
+print(5|6)
+print(5^6)
---
+++ Operators2.py
@@ -0,0 +1,33 @@
+'''a=10
+b=20
+c=100 if a<b else 1000
+print(c)'''
+
+#print largest number among two numbers
+'''a=int(input())
+b=int(input())
+print(a) if a>b else print(b)'''
+
+#print largest number among three numbers
+'''a=int(input())
+b=int(input())
+c=int(input())
+d=a if a>b & a>c else b if b>c else c
+print(d)'''
+
+#membership operator
+'''s="Pankaj"
+print('P' in s)
+
+t=(1,2,3,"Pankaj")
+print('Pankaj' in t)'''
+
+'''a=10
+b=20
+c=30
+print(a,b,c,sep='')'''
+
+print("hello",end=' ')
+print("Good evening",end=' ')
+print("Tata")
+
